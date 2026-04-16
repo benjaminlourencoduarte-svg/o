01931803b1b698a0fc74559ec4a527c45592584f
benjamin <benjaminlourencoduarte@gmail.com>
2026-04-16 18:30:45 -0300

    Rename window title in quiet city script

---
+++ a quiet city script.lua
@@ -0,0 +1,3 @@
+local mixaUiLIB = loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/WfTHwCvXlhE0NZAm/raw"))()-- creating the library
+lib = mixaUiLIB
+local window = lib:CreateWindow("quiet city-luma_hub") -- use this to create a window
